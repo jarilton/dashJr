dd6dc0bf22610962d18057f5efd6279c7b70010c
Jarilton <jariltonpereira02@hotmail.com>
2022-02-21 14:33:18 -0300

    Pág de login com chakra (css no próprio arquivo).

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,52 @@
+import { Button, Flex, FormControl, FormLabel, Input, Stack } from "@chakra-ui/react";
+
 export default function Home() {
   return (
-    <>
-      <h1>Dashboard</h1>
-
-      <p>Config do projeto em nextJS</p>
-    </>
+    <Flex
+      width="100vw"
+      height="100vh"
+      align="center"
+      justify="center"
+    >
+      <Flex
+        as="form"
+        width="100%"
+        maxWidth={360}
+        bg="gray.700"
+        p="8"
+        borderRadius={8}
+        flexDir="column"
+      >
+        <Stack spacing="4">
+          <FormControl>
+            <FormLabel>E-mail</FormLabel>
+            <Input
+              name="email"
+              type="email"
+              focusBorderColor="blue.500"
+              bgColor="gray.900"
+              variant="filled"
+              _hover={{
+                bgColor: 'gray.900'
+              }}
+            />
+          </FormControl>
+          <FormControl>
+            <FormLabel>Password</FormLabel>
+            <Input
+              name="password"
+              type="password"
+              focusBorderColor="blue.500"
+              bgColor="gray.900"
+              variant="filled"
+              _hover={{
+                bgColor: 'gray.900'
+              }}
+            />
+          </FormControl>
+        </Stack>
+        <Button type="submit" mt="6" colorScheme="blue" size="lg">Entrar</Button>
+      </Flex>
+    </Flex>
   )
 }
