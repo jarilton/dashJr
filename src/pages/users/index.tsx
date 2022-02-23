7c209d66eeb4182214eed173e172e47bd91b02f6
Jarilton <jariltonpereira02@hotmail.com>
2022-02-23 18:34:42 -0300

    Página form de usuário

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -8,11 +8,14 @@ export default function UserList() {
   return (
     <Box>
       <Header />
+
       <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
         <Sidebar />
+
         <Box flex="1" borderRadius={8} bg="gray.800" p="8">
           <Flex mb="8" justify="space-between" align="center">
             <Heading size="lg" fontWeight="normal">Usuários</Heading>
+
             <Button
               as="a"
               size="sm"
@@ -114,6 +117,7 @@ export default function UserList() {
             </Tbody>
           </Table>
           <Pagination />
+
         </Box>
       </Flex>
     </Box>
