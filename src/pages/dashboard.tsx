e453e0ee2af4085dc7f0cd9193cb28f2e4972f08
jarilton <jariltonpereira02@hotmail.com>
2022-02-22 23:12:43 -0300

    Página de lista de usuários

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -63,19 +63,24 @@ export default function Dashboard() {
   return (
     <Flex direction="column" h="100vh">
       <Header />
+
       <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
         <Sidebar />
+
         <SimpleGrid flex="1" gap="4" minChildWidth="320px" alignItems="flex-start">
           <Box p="8" bg="gray.800" borderRadius={8} pb="4">
             <Text fontSize="lg" mb="4">Inscritos na semana</Text>
             <Chart options={options} series={series} type="area" height={160} />
           </Box>
+
           <Box p="8" bg="gray.800" borderRadius={8} pb="4">
             <Text fontSize="lg" mb="4">Taxa de abertura</Text>
             <Chart options={options} series={series} type="area" height={160} />
           </Box>
         </SimpleGrid>
+
       </Flex>
+
     </Flex>
   )
 }
