0582cc5cf495c768ec46f1e787f1d3d1abba607d
jarilton <jariltonpereira02@hotmail.com>
2022-02-26 18:45:52 -0300

    Form de autenticação

--- src/components/form/Input.tsx
+++ src/components/form/Input.tsx
@@ -1,3 +1,4 @@
+import { forwardRef, ForwardRefRenderFunction } from "react";
 import { FormControl, FormLabel, Input as ChakraInput, InputProps as ChakraInputProps } from "@chakra-ui/react";
 
 interface InputProps extends ChakraInputProps {
@@ -5,21 +6,25 @@ interface InputProps extends ChakraInputProps {
   label?: string;
 }
 
-export function Input({ name, label, ...rest }: InputProps) {
-  return (
-    <FormControl>
-      {!!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
-      <ChakraInput
-        name="email"
-        type="email"
-        focusBorderColor="blue.500"
-        bgColor="gray.900"
-        variant="filled"
-        _hover={{
-          bgColor: 'gray.900'
-        }}
-        {...rest}
-      />
-    </FormControl>
-  )
-}
+const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps>
+  = ({ name, label, ...rest }, ref) => {
+    return (
+      <FormControl>
+        {!!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
+        <ChakraInput
+          name="email"
+          type="email"
+          focusBorderColor="blue.500"
+          bgColor="gray.900"
+          variant="filled"
+          _hover={{
+            bgColor: 'gray.900'
+          }}
+          ref={ref}
+          {...rest}
+        />
+      </FormControl>
+    )
+  }
+
+export const Input = forwardRef(InputBase)
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,21 @@
 import { Button, Flex, Link, Stack, Text } from "@chakra-ui/react";
+import { SubmitHandler, useForm } from 'react-hook-form'
 import { Input } from "../components/form/Input";
 
-export default function Sign() {
+type SignInFormData = {
+  email: string,
+  password: string,
+}
+
+export default function SignIn() {
+  const { register, handleSubmit, formState } = useForm()
+
+  const handleSignIn: SubmitHandler<SignInFormData> = async (values) => {
+    await new Promise(resolve => setTimeout(resolve, 2000))
+
+    console.log(values);
+  }
+
   return (
     <Flex
       width="100vw"
@@ -17,18 +31,27 @@ export default function Sign() {
         p="8"
         borderRadius={8}
         flexDir="column"
+        onSubmit={handleSubmit(handleSignIn)}
       >
         <Stack spacing="4">
           <Text align="center" fontWeight="bold" fontSize='4xl'>
             Dash Jr
           </Text>
 
-          <Input name="email" type="email" label="E-mail:" />
-          <Input name="password" type="password" label="Senha:" />
+          <Input name="email" type="email" label="E-mail:" {...register('email')} />
+          <Input name="password" type="password" label="Senha:" {...register('password')} />
           <Link align="right">Esqueci minha senha</Link>
 
         </Stack>
-        <Button type="submit" mt="6" colorScheme="blue" size="lg">Entrar</Button>
+        <Button
+          type="submit"
+          mt="6"
+          colorScheme="blue"
+          size="lg"
+          isLoading={formState.isSubmitting}
+        >
+          Entrar
+        </Button>
       </Flex>
     </Flex >
   )
