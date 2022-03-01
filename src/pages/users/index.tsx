2c269a5f867c4b63e224f8f7180bd869bb3cca06
jarilton <jariltonpereira02@hotmail.com>
2022-03-01 17:30:58 -0300

    Configurando react query no projeto

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,22 +1,25 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
+import { Box, Button, Checkbox, Flex, Heading, Icon, Spinner, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from "@chakra-ui/react";
 import Link from "next/link";
 import { useEffect } from "react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
+import { useQuery } from 'react-query'
+
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList() {
+  const { data, isLoading, error } = useQuery('users', async () => {
+    const response = await fetch('http://localhost:3000/api/users')
+    const data = await response.json()
+
+    return data;
+  })
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
   })
-
-  useEffect(() => {
-    fetch('http://localhost:3000/api/users')
-      .then(response => response.json())
-      .then(data => console.log(data))
-  }, [])
 
   return (
     <Box>
@@ -41,98 +44,110 @@ export default function UserList() {
               </Button>
             </Link>
           </Flex>
-          <Table colorScheme="whiteAlpha">
-            <Thead>
-              <Tr>
-                <Th px={["4", "4", "6"]} color="gray.300" width="8">
-                  <Checkbox colorScheme="blue" />
-                </Th>
-                <Th>Usuário</Th>
-                {isWideVersion && <Th>Data de cadastro</Th>}
-                <Th width="10"></Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td px={["4", "4", "6"]} >
-                  <Checkbox colorScheme="blue" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Thalia Favaro</Text>
-                    <Text fontSize="sm" color="gray.300">thaliasouzafavaro@gmail.com</Text>
-                  </Box>
-                </Td>
-                {isWideVersion && <Td>14 de agosto, 1997</Td>}
-                <Td >
-                  <Button
-                    as="a"
-                    size="sm"
-                    fontSize="sm"
-                    colorScheme="yellow"
-                    leftIcon={<Icon
-                      as={RiPencilLine} fontSize="16" />}
-                  >
-                    Editar
-                  </Button>
-                </Td>
-              </Tr>
-            </Tbody>
-            <Tbody>
-              <Tr>
-                <Td px={["4", "4", "6"]} >
-                  <Checkbox colorScheme="blue" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Alisson Macedo</Text>
-                    <Text fontSize="sm" color="gray.300">alisson@gmail.com</Text>
-                  </Box>
-                </Td>
-                {isWideVersion && <Td>25 de setembro, 1971</Td>}
-                <Td >
-                  <Button
-                    as="a"
-                    size="sm"
-                    fontSize="sm"
-                    colorScheme="yellow"
-                    leftIcon={<Icon
-                      as={RiPencilLine} fontSize="16" />}
-                  >
-                    Editar
-                  </Button>
-                </Td>
-              </Tr>
-            </Tbody>
-            <Tbody>
-              <Tr>
-                <Td px={["4", "4", "6"]} >
-                  <Checkbox colorScheme="blue" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">Jarilton Junior</Text>
-                    <Text fontSize="sm" color="gray.300">jariltonpereira02@hotmail.com</Text>
-                  </Box>
-                </Td>
-                {isWideVersion && <Td>01 de março, 1998</Td>}
-                <Td >
-                  <Button
-                    as="a"
-                    size="sm"
-                    fontSize="sm"
-                    colorScheme="yellow"
-                    leftIcon={<Icon
-                      as={RiPencilLine} fontSize="16" />}
-                  >
-                    Editar
-                  </Button>
-                </Td>
-              </Tr>
-            </Tbody>
-          </Table>
-          <Pagination />
 
+          {isLoading ? (
+            <Flex justify="center">
+              <Spinner />
+            </Flex>
+          ) : error ? (
+            <Flex justify="center">
+              <Text>Falha ao obter dados dos usuários</Text>
+            </Flex>
+          ) : (
+            <>
+              <Table colorScheme="whiteAlpha">
+                <Thead>
+                  <Tr>
+                    <Th px={["4", "4", "6"]} color="gray.300" width="8">
+                      <Checkbox colorScheme="blue" />
+                    </Th>
+                    <Th>Usuário</Th>
+                    {isWideVersion && <Th>Data de cadastro</Th>}
+                    <Th width="10"></Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  <Tr>
+                    <Td px={["4", "4", "6"]} >
+                      <Checkbox colorScheme="blue" />
+                    </Td>
+                    <Td>
+                      <Box>
+                        <Text fontWeight="bold">Thalia Favaro</Text>
+                        <Text fontSize="sm" color="gray.300">thaliasouzafavaro@gmail.com</Text>
+                      </Box>
+                    </Td>
+                    {isWideVersion && <Td>14 de agosto, 1997</Td>}
+                    <Td >
+                      <Button
+                        as="a"
+                        size="sm"
+                        fontSize="sm"
+                        colorScheme="yellow"
+                        leftIcon={<Icon
+                          as={RiPencilLine} fontSize="16" />}
+                      >
+                        Editar
+                      </Button>
+                    </Td>
+                  </Tr>
+                </Tbody>
+                <Tbody>
+                  <Tr>
+                    <Td px={["4", "4", "6"]} >
+                      <Checkbox colorScheme="blue" />
+                    </Td>
+                    <Td>
+                      <Box>
+                        <Text fontWeight="bold">Alisson Macedo</Text>
+                        <Text fontSize="sm" color="gray.300">alisson@gmail.com</Text>
+                      </Box>
+                    </Td>
+                    {isWideVersion && <Td>25 de setembro, 1971</Td>}
+                    <Td >
+                      <Button
+                        as="a"
+                        size="sm"
+                        fontSize="sm"
+                        colorScheme="yellow"
+                        leftIcon={<Icon
+                          as={RiPencilLine} fontSize="16" />}
+                      >
+                        Editar
+                      </Button>
+                    </Td>
+                  </Tr>
+                </Tbody>
+                <Tbody>
+                  <Tr>
+                    <Td px={["4", "4", "6"]} >
+                      <Checkbox colorScheme="blue" />
+                    </Td>
+                    <Td>
+                      <Box>
+                        <Text fontWeight="bold">Jarilton Junior</Text>
+                        <Text fontSize="sm" color="gray.300">jariltonpereira02@hotmail.com</Text>
+                      </Box>
+                    </Td>
+                    {isWideVersion && <Td>01 de março, 1998</Td>}
+                    <Td >
+                      <Button
+                        as="a"
+                        size="sm"
+                        fontSize="sm"
+                        colorScheme="yellow"
+                        leftIcon={<Icon
+                          as={RiPencilLine} fontSize="16" />}
+                      >
+                        Editar
+                      </Button>
+                    </Td>
+                  </Tr>
+                </Tbody>
+              </Table>
+              <Pagination />
+            </>
+          )}
         </Box>
       </Flex>
     </Box>
